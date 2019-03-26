805fddff73e7f3c9930c1171b98bc08cc4d26712
Benkassou Ali <ali.benkassou@insa-rouen.fr>
2019-03-26 17:50:08 +0100

    Major bug spotted, maybe fixed

--- model/model_fn.py
+++ model/model_fn.py
@@ -19,22 +19,22 @@ def cnn_model_fn(features, labels, mode):
     # Useful variables
     num_frames = 29
     num_classes = 500
-    
-    # Input layer 
+
+    # Input layer
     input_layer = tf.reshape(features, [-1, 64, 64, num_frames])
-    
-    # Convolutional Layer #1 : 
+
+    # Convolutional Layer #1 :
     # on every frame separately , shared weights
     conv1 = list()
     for i in range(num_frames) :
         conv1.append(tf.layers.conv2d(
             inputs=tf.reshape(
-                input_layer[:, :, :, i], 
+                input_layer[:, :, :, i],
                 [-1, 64, 64, 1]),
-            filters=48, 
+            filters=48,
             kernel_size=[3, 3],
             padding="valid",
-            activation=tf.nn.relu, 
+            activation=tf.nn.relu,
             name="conv1",
             reuse=tf.AUTO_REUSE))
     # Pooling Layer #1 :
@@ -51,18 +51,20 @@ def cnn_model_fn(features, labels, mode):
         values=pool1,
         axis=3,
         name="concat")
-    
+
     # Convolutional Layer to reduce dimension
     conv_dim = tf.layers.conv2d(
         inputs=layer1,
         filters=92,
+        activation=tf.nn.relu,
         kernel_size=[1, 1],
         name="conv_dim")
-    
+
     # Convolutional and Pooling Layers #2
     conv2 = tf.layers.conv2d(
         inputs=conv_dim,
         filters=256,
+        activation=tf.nn.relu,
         kernel_size=[3, 3],
         name="conv2")
     pool2 = tf.layers.max_pooling2d(
@@ -70,72 +72,75 @@ def cnn_model_fn(features, labels, mode):
         pool_size=[2, 2],
         strides=2,
         name="pool2")
-    
+
     # Convolutional Layer #3
     conv3 = tf.layers.conv2d(
         inputs = pool2,
         filters=512,
+        activation=tf.nn.relu,
         kernel_size=[3, 3],
         name="conv3")
-    
+
     # Convolutional Layer #4
     conv4 = tf.layers.conv2d(
         inputs = conv3,
         filters=512,
+        activation=tf.nn.relu,
         kernel_size=[3, 3],
         name="conv4")
-    
+
     # Convolutional and Pooling Layers #5
     conv5 = tf.layers.conv2d(
         inputs=conv4,
         filters=512,
         kernel_size=[3, 3],
+        activation=tf.nn.relu,
         name="conv5")
     pool5 = tf.layers.max_pooling2d(
         inputs=conv5,
         pool_size=[2, 2],
         strides=2,
         name="pool5")
-    
+
     # Flatten the feature map
-    pool5_flat = tf.reshape(pool5, 
+    pool5_flat = tf.reshape(pool5,
                             [-1, 8192],
                             name="pool5_flat")
-    
+
     # Dense Layer
     dense = tf.layers.dense(
         inputs=pool5_flat,
         units=4096,
         activation=tf.nn.relu,
         name="dense")
-    
+
     # IF OVERFITTING, USE DROPOUT HERE
-    
+
     # Logits Layer
     logits = tf.layers.dense(
         inputs=dense,
         units=num_classes,
         name="logits")
-    
+
     # Generate predictions
     predictions = {
-        "classes": tf.argmax(input=logits, 
-                             axis=1, 
+        "classes": tf.argmax(input=logits,
+                             axis=1,
                              name="argmax"),
-        "probabilities": tf.nn.softmax(logits=logits, 
+        "probabilities": tf.nn.softmax(logits=logits,
                                        name="softmax")
     }
-    
+
     # Prediction stops here
     if mode == tf.estimator.ModeKeys.PREDICT :
-        return tf.estimator.EstimatorSpec(mode=mode, 
+        return tf.estimator.EstimatorSpec(mode=mode,
                                           predictions=predictions)
-    
+
     # Calculate loss
     loss = tf.losses.sparse_softmax_cross_entropy(
         labels=labels,
         logits=logits)
-    
+
     # Evaluation metrics
     accuracy = tf.metrics.accuracy(labels=labels,
                                    predictions=predictions["classes"],
@@ -143,21 +148,21 @@ def cnn_model_fn(features, labels, mode):
     metrics = {'accuracy' : accuracy}
     tf.summary.scalar('accuracy', accuracy[1])
     tf.summary.scalar('loss', loss)
-    
+
     # Evaluation stops here
     if mode == tf.estimator.ModeKeys.EVAL :
         return tf.estimator.EstimatorSpec(
             mode=mode,
             loss=loss,
             eval_metric_ops=metrics)
-    
+
     # Training stops here
     starter_learning_rate = 0.01
     global_step = tf.train.get_global_step()
-    learning_rate = tf.train.exponential_decay(starter_learning_rate, 
+    learning_rate = tf.train.exponential_decay(starter_learning_rate,
                                                global_step,
-                                               10000, 
-                                               0.76, 
+                                               10000,
+                                               0.76,
                                                staircase=False)
     tf.summary.scalar('learning_rate', learning_rate)
     optimizer = tf.train.MomentumOptimizer(
