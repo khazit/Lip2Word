e3dbf18bdc20a353e2c785906139055b122a4dcd
Benkassou Ali <ali.benkassou@insa-rouen.fr>
2019-03-26 16:38:31 +0100

    Helper function to compute conv outputs size

---
+++ helpers/conv_size.py
@@ -0,0 +1,15 @@
+"""
+Given an input size and some parameters, compute the output size of a convolution
+"""
+
+import sys
+
+input_size = int(sys.argv[1])
+window_size = int(sys.argv[2])
+stride = int(sys.argv[3])
+padding_type = sys.argv[4]
+
+if padding_type == "valid" :
+    output_size = round( (input_size-window_size) / stride) + 1
+
+print("Output size : {}".format(output_size))
