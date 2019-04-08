216307a7aa358c861560e580ddc360ed99138f0d
Benkassou Ali <ali.benkassou@insa-rouen.fr>
2019-04-09 00:50:34 +0200

    added help

--- helpers/conv_size.py
+++ helpers/conv_size.py
@@ -4,12 +4,13 @@ Given an input size and some parameters, compute the output size of a convolutio
 
 import sys
 
-input_size = int(sys.argv[1])
-window_size = int(sys.argv[2])
-stride = int(sys.argv[3])
-padding_type = sys.argv[4]
-
-if padding_type == "valid" :
-    output_size = round( (input_size-window_size) / stride) + 1
-
-print("Output size : {}".format(output_size))
+if (sys.argv[1] == "h") :
+    print("python3 conv_size.py [input_size] [window_size] [stride] [padding_type]")
+else :
+    input_size = int(sys.argv[1])
+    window_size = int(sys.argv[2])
+    stride = int(sys.argv[3])
+    padding_type = sys.argv[4]
+    if padding_type == "valid" :
+        output_size = round( (input_size-window_size) / stride) + 1
+    print("Output size : {}".format(output_size))
