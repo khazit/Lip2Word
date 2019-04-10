8a764cf329cfb34523d69f1d482594e5dce15a92
Benkassou Ali <ali.benkassou@insa-rouen.fr>
2019-04-10 17:43:13 +0200

    Inception model compatibility

--- parameter_count.py
+++ parameter_count.py
@@ -8,16 +8,23 @@ https://gist.github.com/dalgu90/a9952dfd372cbe1cdc529b204329e189
 import sys
 import tensorflow as tf
 import numpy as np
-from model.model_fn import cnn_model_fn
+from model.model_fn.vgg import vgg_model_fn
+from model.model_fn.inception import inception_model_fn
 
-if len(sys.argv) == 2:
+
+if len(sys.argv) == 3:
     model_dir = sys.argv[1]
 else:
-    print('Usage: python count_ckpt_param.py path-to-ckpt')
+    print('Usage: python count_ckpt_param.py path_to_chkpt')
     sys.exit(1)
 
+if sys.argv[2] == "vgg" :
+    model_fn = vgg_model_fn
+elif sys.argv[2] == "inception" :
+    model_fn = inception_model_fn
+
 cnn_classifier = tf.estimator.Estimator(
-    model_fn=cnn_model_fn,
+    model_fn=model_fn,
     model_dir=model_dir)
 
 reader = tf.train.NewCheckpointReader(tf.train.latest_checkpoint(model_dir))
