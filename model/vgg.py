0aa090e38d86f5dd81eaaf4fdbe2b52657a48e31
Benkassou Ali <ali.benkassou@insa-rouen.fr>
2019-05-02 20:57:16 +0200

    _build_model function

--- model/vgg.py
+++ model/vgg.py
@@ -13,12 +13,135 @@ def vgg_model_fn(features, labels, mode, params):
         - Custom Estimator
     """
     # Useful variables
-    num_frames = 29
     num_classes = params["num_classes"]
     if (mode == tf.estimator.ModeKeys.TRAIN) :
         is_training = tf.constant(True, dtype=tf.bool)
     else :
         is_training = tf.constant(False, dtype=tf.bool)
+
+    # Compute the logits
+    logits=_build_model(features, num_classes, is_training)
+
+    # Generate predictions
+    predictions = {
+        "classes": tf.argmax(
+            input=logits,
+            axis=1,
+            name="argmax"
+        ),
+        "probabilities": tf.nn.softmax(
+            logits=logits,
+            name="softmax"
+        )
+    }
+
+    # Prediction stops here
+    if mode == tf.estimator.ModeKeys.PREDICT :
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=predictions
+    )
+
+    # Calculate loss
+    loss = tf.losses.sparse_softmax_cross_entropy(
+        labels=labels,
+        logits=logits
+    )
+
+    # Compute evaluation metrics
+    accuracy = tf.metrics.accuracy(
+        labels=labels,
+        predictions=predictions["classes"],
+        name='acc_op'
+    )
+    topk_accuracy = tf.metrics.mean(
+        tf.nn.in_top_k(
+            predictions=logits,
+            targets=labels,
+            k=10
+        )
+    )
+    metrics = {
+        'accuracy': accuracy,
+        'topk_accuracy': topk_accuracy
+    }
+
+    # Display stuff on Tensorboard
+    tf.summary.scalar('accuracy', accuracy[1])
+    tf.summary.scalar('topk_accuracy', topk_accuracy[1])
+    tf.summary.scalar('loss', loss)
+    # Uncomment to display image inputs on Tensorboard
+    # tf.summary.image(
+    #     tensor=tf.reshape(
+    #         features[:, :, :, 1],
+    #         [-1, 64, 64, 1]),
+    #     name="viz")
+
+    # Logging hook
+    hook = tf.train.LoggingTensorHook(
+        tensors = {
+            "accuracy": accuracy[0],
+            "topk_accuracy": topk_accuracy[0]
+        },
+        every_n_iter=100
+    )
+
+    # Evaluation stops here
+    if mode == tf.estimator.ModeKeys.EVAL :
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            eval_metric_ops=metrics,
+            evaluation_hooks=[hook]
+        )
+
+    # Learning rate
+    starter_learning_rate = params["starter_learning_rate"]
+    global_step = tf.train.get_global_step()
+    learning_rate = tf.train.exponential_decay(
+        learning_rate=starter_learning_rate,
+        global_step=global_step,
+        decay_steps=params["decay_steps"],
+        decay_rate=params["decay_rate"],
+        staircase=False
+    )
+
+    # Display more stuff on Tensorboard (training related)
+    tf.summary.scalar('learning_rate', learning_rate)
+
+    # Optimizer specifications
+    optimizer = tf.train.MomentumOptimizer(
+        learning_rate=learning_rate,
+        momentum=params["optimizer_momentum"]
+    )
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=global_step
+    )
+
+    # Add the update ops for the moving_mean and moving_variance of batchnorm
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    train_op = tf.group([train_op, update_ops])
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        loss=loss,
+        train_op=train_op,
+        training_hooks=[hook]
+    )
+
+def _build_model(features, num_classes, is_training) :
+    """
+    Build the graph for the Multiple Tower architecture
+    Args :
+        - features : Tensor of the inputs
+        - num_classes : number of number of classes
+        - is_training : whether it's training mode or inference mode
+    Returns :
+        - logits
+    """
+    # Useful variables
+    num_frames = 29
 
     # Convolutional Layer #1 :
     # on every frame separately , shared weights
@@ -158,115 +281,10 @@ def vgg_model_fn(features, labels, mode, params):
 
     # IF OVERFITTING, USE DROPOUT HERE
 
-    # Logits Layer
+    # Logits
     logits = tf.layers.dense(
         inputs=dense,
         units=num_classes,
         name="logits"
     )
-
-    # Generate predictions
-    predictions = {
-        "classes": tf.argmax(
-            input=logits,
-            axis=1,
-            name="argmax"),
-        "probabilities": tf.nn.softmax(
-            logits=logits,
-            name="softmax")
-    }
-
-    # Prediction stops here
-    if mode == tf.estimator.ModeKeys.PREDICT :
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            predictions=predictions
-    )
-
-    # Calculate loss
-    loss = tf.losses.sparse_softmax_cross_entropy(
-        labels=labels,
-        logits=logits
-    )
-
-    # Compute evaluation metrics
-    accuracy = tf.metrics.accuracy(
-        labels=labels,
-        predictions=predictions["classes"],
-        name='acc_op'
-    )
-    topk_accuracy = tf.metrics.mean(
-        tf.nn.in_top_k(
-            predictions=logits,
-            targets=labels,
-            k=10
-        )
-    )
-    metrics = {
-        'accuracy': accuracy,
-        'topk_accuracy': topk_accuracy
-    }
-
-    # Display stuff on Tensorboard
-    tf.summary.scalar('accuracy', accuracy[1])
-    tf.summary.scalar('topk_accuracy', topk_accuracy[1])
-    tf.summary.scalar('loss', loss)
-    # Uncomment to display image inputs on Tensorboard
-    # tf.summary.image(
-    #     tensor=tf.reshape(
-    #         features[:, :, :, 1],
-    #         [-1, 64, 64, 1]),
-    #     name="viz")
-
-    # Logging hook
-    hook = tf.train.LoggingTensorHook(
-        tensors = {
-            "accuracy": accuracy[0],
-            "topk_accuracy": topk_accuracy[0]
-        },
-        every_n_iter=100
-    )
-
-    # Evaluation stops here
-    if mode == tf.estimator.ModeKeys.EVAL :
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            loss=loss,
-            eval_metric_ops=metrics,
-            evaluation_hooks=[hook]
-        )
-
-    # Learning rate
-    starter_learning_rate = params["starter_learning_rate"]
-    global_step = tf.train.get_global_step()
-    learning_rate = tf.train.exponential_decay(
-        learning_rate=starter_learning_rate,
-        global_step=global_step,
-        decay_steps=params["decay_steps"],
-        decay_rate=params["decay_rate"],
-        staircase=False
-    )
-
-    # Display more stuff on Tensorboard (training related)
-    tf.summary.scalar('learning_rate', learning_rate)
-
-    # Optimizer specifications
-    optimizer = tf.train.MomentumOptimizer(
-        learning_rate=learning_rate,
-        momentum=params["optimizer_momentum"]
-    )
-    train_op = optimizer.minimize(
-        loss=loss,
-        global_step=global_step
-    )
-
-    # Add the update ops for the moving_mean and moving_variance of batchnorm
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    train_op = tf.group([train_op, update_ops])
-
-    return tf.estimator.EstimatorSpec(
-        mode=mode,
-        loss=loss,
-        train_op=train_op,
-        training_hooks=[hook]
-    )
+    return logits
